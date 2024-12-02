3a62c7f8082ff97a881f8b2cc957ee2e7728965e
Dumindu Lakshan <104787847+DuminduBandara@users.noreply.github.com>
2024-12-02 09:48:16 +0530

    updated

---
+++ src/pages/AddDestination.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+export default function AddDestination() {
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h4" sx={{fontSize: '1.6rem'}}>Add New Destination</Typography>
+      </Box>
+      <Box>
+        
+      </Box>
+    </Box>
+  );
+}
